11ac04b533f42b00a838cba7fa34e1a16b1ed9c5
irfan ayaz <byirfanayaz@gmail.com>
2024-02-15 23:02:59 +0300

    String Ifadeyi int Diziye Cevirir

---
+++ Dizi.cs
@@ -0,0 +1,26 @@
+﻿namespace Matematik;
+
+public class Dizi
+{
+/// <summary>
+/// Virgülle ayrılmış olarak girilen string diziyi sayısal diziye çevirir.
+/// </summary>
+/// <param name="ifade"></param>
+/// <returns></returns>
+    public int [] IfadeyiDiziyeCevir(string ifade)
+    {
+     string [] bolunmusIfade= ifade.Split(',');
+     int n = bolunmusIfade.Length;
+     //sayısal dizi
+     int [] sayisalDizi = new int[n];
+
+     // Ayrıştırma
+     for (int i = 0; i < n; i++)
+     {
+        sayisalDizi[i] = Convert.ToInt32(bolunmusIfade[i]);
+        System.Console.WriteLine("dizi [{0}] = {1}", i ,sayisalDizi[i]); 
+     }
+     return sayisalDizi;
+    }
+
+}
