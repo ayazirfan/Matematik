b0418cf3d165699ac84b2bea9aa4f9b823447156
irfan ayaz <byirfanayaz@gmail.com>
2024-02-15 22:17:14 +0300

    Faktoriyel Hesaplama

---
+++ Faktoriyel.cs
@@ -0,0 +1,15 @@
+﻿namespace Matematik;
+
+public class Faktoriyel
+{
+    public static int FaktoriyelAl (int n)
+    {
+        if (n<=1)
+        return 1;
+        int f =1;
+        for (int i = 2; i <= n; i++)
+        f*=i;
+        return f;              
+    }
+
+}
