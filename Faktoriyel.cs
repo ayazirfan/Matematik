45eaf8ede55768b90f81b6122e04bb7b74d43c68
irfan ayaz <byirfanayaz@gmail.com>
2024-02-17 16:15:09 +0300

    Faktoriyel hesaplama

--- Faktoriyel.cs
+++ Faktoriyel.cs
@@ -9,7 +9,8 @@ public class Faktoriyel
         int f =1;
         for (int i = 2; i <= n; i++)
         f*=i;
-        return f;              
+        
+        return f ;               
     }
 
 }
